1ba7df415fb87788474b0aa820f1eb824e4fecab
github-Tanyap1 <github-Tanya.Pachkalov@MyGeorgian.ca>
2022-03-26 17:17:59 -0400

    more notes

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,15 +1,16 @@
 function Delete(){
-    return confirm ('Confirm Deletion')
+    return confirm ('Confirm Deletion')//confirmation of deletion from the class
 }
 
 //const SalaryCalculator = require("salary-calculator");
 //filed app
 
-
+//calc salary 
 function calcSalary(){
-    let wage = parseFloat(document.getElementById('wage').value); 
-    let hours = parseFloat(document.getElementById('hours').value);   
-    let calculate = wage * hours * 52;
-    document.getElementById('results').innerHTML = "$ " + calculate;
+    let wage = parseFloat(document.getElementById('wage').value); //this is the wage 
+    let hours = parseFloat(document.getElementById('hours').value);   //this is the hours
+    let calculate = wage * hours * 52;//calculation by 52 week a year
+    document.getElementById('results').innerHTML = "$ " + calculate; // click reult plus the $ sign 
+
     }
     
